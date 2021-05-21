dbb8e619984b9bcc340468dad40297655768b90f
shanxin <252345449@qq.com>
2021-05-21 11:21:16 +0800

    first commit

---
+++ src/test/java/xyz/mxin/springaopjwt/SpringAopJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.mxin.springaopjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
